0019cdc4fc4e77b3d341e1fca8b3bbb2b9b7317d
Ankita Sinha <ankita.sinha@mphasis.com>
2021-01-13 10:31:53 +0530

    Added tables

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import EnhancedTable from './EnhancedTable';
 
 export default function Table() {
     return (
         <Container maxWidth="lg">
         <Typography component="div" variant="h4" style={{ backgroundColor: '#fff', height: '100vh', marginTop: '120px' }} > 
         Table Component goes here
+        <EnhancedTable/>
          </Typography>
+         
       </Container>
     )
 }
